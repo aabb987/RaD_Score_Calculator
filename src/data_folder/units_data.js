75607fc353e47d678608231083444d5e6cf1a172
aabb987 <95989405+aabb987@users.noreply.github.com>
2025-05-11 22:53:10 +0800

    Add files via upload

---
+++ src/data_folder/units_data.js
@@ -0,0 +1,270 @@
+//手部槍械武器
+export const arm_weapon_ranged = [
+    { id: "01", name: "MA-J-200 RANSETSU-RF", score: "1" },
+    { id: "02", name: "LR-036 CURTIS", score: "2" },
+    { id: "03", name: "LR-037 HARRIS", score: "3" },
+    { id: "04", name: "RF-024 TURNER", score: "0" },
+    { id: "05", name: "RF-025 SCUDDER", score: "0" },
+    { id: "06", name: "MA-J-201 RANSETSU-AR", score: "-2" },
+    { id: "07", name: "MG-014 LUDLOW", score: "0" },
+    { id: "08", name: "DF-MG-02 CHANG-CHEN", score: "1" },
+    { id: "09", name: "WR-0555 ATTACHE", score: "1" },
+    { id: "10", name: "MA-E-210 ETSUJIN", score: "5" },
+    { id: "11", name: "DF-GA-08 HU-BEN", score: "1" },
+    { id: "12", name: "SG-026 HALDEMAN", score: "3" },
+    { id: "13", name: "SG-027 ZIMMERMAN", score: "5" },
+    { id: "14", name: "WR-0777 SWEET SIXTEEN", score: "2" },
+    { id: "15", name: "HG-003 COQUILLETT", score: "3" },
+    { id: "16", name: "HG-004 DUCKETT", score: "4" },
+    { id: "17", name: "MA-E-211 SAMPU", score: "1" },
+    { id: "18", name: "EL-PW-00 VIENTO", score: "2" },
+    { id: "19", name: "VP-66EG", score: "5" },
+    { id: "20", name: "DF-BA-06 XUAN-GE", score: "2" },
+    { id: "21", name: "MAJESTIC", score: "5" },
+    { id: "22", name: "LITTLE GEM", score: "0" },
+    { id: "23", name: "44-141 JVLN ALPHA", score: "3" },
+    { id: "24", name: "DF-GR-07 GOU-CHEN", score: "1" },
+    { id: "25", name: "DIZZY", score: "2" },
+    { id: "26", name: "IRIDIUM", score: "0" },
+    { id: "27", name: "MA-T-222 KYORAI", score: "0" },
+    { id: "28", name: "MA-T-223 KYORIKU", score: "0" },
+    { id: "29", name: "WS-1200 THERAPIST", score: "4" },
+    { id: "30", name: "WB-0000 BAD COOK", score: "1" },
+    { id: "31", name: "VP-66LR", score: "2" },
+    { id: "32", name: "VE-66LRA", score: "4" },
+    { id: "33", name: "VE-66LRB", score: "5" },
+    { id: "34", name: "VP-66LS", score: "1" },
+    { id: "35", name: "WUERGER-66E", score: "3" },
+    { id: "36", name: "VP-66LH", score: "1" },
+    { id: "37", name: "Vvc-760PR", score: "2" },
+    { id: "38", name: "IA-C01W1 NEBULA", score: "5" },
+    { id: "39", name: "IA-C01W6 NB-REDSHIFT", score: "4" },
+    { id: "40", name: "IB-C03W1 WLT 011", score: "3" },
+    { id: "41", name: "44-142 KRSV", score: "3" },
+    { id: "42", name: "HI-16 GU-Q1", score: "1" },
+    { id: "43", name: "HI-18 GU-A2", score: "2" },
+    { id: "44", name: "HML-G2-P19MLT-04", score: "2" },
+    { id: "45", name: "HML-G3-P08SPL-06", score: "2" },
+    { id: "46", name: "WS-5000 APERITIF", score: "2" },
+    { id: "47", name: "PFAU-66D", score: "3" }
+];
+
+//手部近戰武器
+export const arm_weapon_melee = [
+    { id: "01", name: "PB-033M ASHMEAD", score: "0" },
+    { id: "02", name: "DF-ET-09 TAI-YANG-SHOU", score: "4" },
+    { id: "03", name: "WB-0010 DOUBLE TROUBLE", score: "-1" },
+    { id: "04", name: "VP-67EB", score: "-1" },
+    { id: "05", name: "VP-67LD", score: "2" },
+    { id: "06", name: "Vvc-770LB", score: "0" },
+    { id: "07", name: "Vvc-774LS", score: "2" },
+    { id: "08", name: "VE-67LLA", score: "-1" },
+    { id: "09", name: "44-143 HMMR", score: "4" },
+    { id: "10", name: "HI-32: BU-TT/A", score: "2" },
+    { id: "11", name: "IA-C01W2: MOONLIGHT", score: "3" },
+    { id: "12", name: "IA-C01W7: ML-REDSHIFT", score: "3" },
+    { id: "13", name: "IB-C03W2: WLT 101", score: "3" }
+];
+
+// 背部武器
+export const back_weapon = [
+    { id: "01", name: "DF-GA-09 SHAO-WEI", score: "0" },
+    { id: "02", name: "SB-033M MORLEY", score: "2" },
+    { id: "03", name: "EARSHOT", score: "4" },
+    { id: "04", name: "SONGBIRDS", score: "0" },
+    { id: "05", name: "VE-60SNA", score: "2" },
+    { id: "06", name: "VP-60LCS", score: "5" },
+    { id: "07", name: "VE-60LCA", score: "5" },
+    { id: "08", name: "VE-60LCB", score: "5" },
+    { id: "09", name: "VP-60LCD", score: "5" },
+    { id: "10", name: "FASAN/60E", score: "5" },
+    { id: "11", name: "KRANICH/60Z", score: "2" },
+    { id: "12", name: "EULE/60D", score: "0" },
+    { id: "13", name: "IA-C01W3: AURORA", score: "3" },
+    { id: "14", name: "BML-G1/P20MLT-04", score: "2" },
+    { id: "15", name: "BML-G2/P03MLT-06", score: "2" },
+    { id: "16", name: "BML-G2/P05MLT-10", score: "3" },
+    { id: "17", name: "BML-G2/P19SPL-12", score: "2" },
+    { id: "18", name: "BML-G2/P16SPL-08", score: "2" },
+    { id: "19", name: "BML-G2/P17SPL-16", score: "2" },
+    { id: "20", name: "BML-G1/P31DUO-02", score: "3" },
+    { id: "21", name: "BML-G1/P32DUO-03", score: "2" },
+    { id: "22", name: "BML-G2/P08DUO-03", score: "0" },
+    { id: "23", name: "BML-G1/P01VTC-04", score: "-1" },
+    { id: "24", name: "BML-G1/P03VTC-08", score: "-1" },
+    { id: "25", name: "BML-G1/P07VTC-12", score: "-1" },
+    { id: "26", name: "BML-G3/P04ACT-01", score: "1" },
+    { id: "27", name: "BML-G3/P05ACT-02", score: "0" },
+    { id: "28", name: "BML-G1/P29CNT", score: "-1" },
+    { id: "29", name: "WR-0999 DELIVERY BOY", score: "-1" },
+    { id: "30", name: "WS-5001 SOUP", score: "4" },
+    { id: "31", name: "45-091 JVLN BETA", score: "4" },
+    { id: "32", name: "EL-PW-01 TRUENO", score: "3" },
+    { id: "33", name: "Vvc-703PM", score: "4" },
+    { id: "34", name: "Vvc-706PM", score: "3" },
+    { id: "35", name: "Vvc-70VMP", score: "5" },
+    { id: "36", name: "IB-C03W3: NGI 006", score: "5" },
+    { id: "37", name: "BO-044 HUXLEY", score: "4" },
+    { id: "38", name: "45-091 ORBT", score: "4" },
+    { id: "39", name: "VP-60LT", score: "-1" },
+    { id: "40", name: "Vvc-700LD", score: "5" }
+];
+
+// 背部盾牌武器
+export const back_weapon_shield = [
+    { id: "01", name: "VP-61PS", score: "5" },
+    { id: "02", name: "SI-24: SU-Q5", score: "4" },
+    { id: "03", name: "SI-27: SU-R8", score: "0" },
+    { id: "04", name: "VP-61PB", score: "5" },
+    { id: "05", name: "SI-29: SU-TT/C", score: "3" },
+    { id: "06", name: "VE-61PSA", score: "3" },
+    { id: "07", name: "IB-C03W4: NGI 028", score: "0" }
+];
+
+// 頭部 
+export const parts_head = [
+    { id: "01", name: "AH-J-124 BASHO", score: "1" },
+    { id: "02", name: "AH-J-124/RC JAILBREAK", score: "-1" },
+    { id: "03", name: "HD-011 MELANDER", score: "1" },
+    { id: "04", name: "HD-033M VERRILL", score: "5" },
+    { id: "05", name: "HD-012 MELANDER C3", score: "2" },
+    { id: "06", name: "DF-HD-08 TIAN-QIANG", score: "-1" },
+    { id: "07", name: "VP-44S", score: "1" },
+    { id: "08", name: "VP-44D", score: "5" },
+    { id: "09", name: "NACHTREIHER/44E", score: "1" },
+    { id: "10", name: "KASUAR/44Z", score: "3" },
+    { id: "11", name: "LAMMERGEIER/44F", score: "-1" },
+    { id: "12", name: "VE-44A", score: "2" },
+    { id: "13", name: "VE-44B", score: "4" },
+    { id: "14", name: "HC-2000 FINDER EYE", score: "0" },
+    { id: "15", name: "HC-2000/BC SHADE EYE", score: "4" },
+    { id: "16", name: "HC-3000 WRECKER", score: "0" },
+    { id: "17", name: "HS-5000 APPETIZER", score: "3" },
+    { id: "18", name: "EL-TH-10 FIRMEZA", score: "0" },
+    { id: "19", name: "EL-PH-00 ALBA", score: "0" },
+    { id: "20", name: "20-081 MIND ALPHA", score: "4" },
+    { id: "21", name: "20-082 MIND BETA", score: "4" },
+    { id: "22", name: "IA-C01H: EPHEMERA", score: "5" },
+    { id: "23", name: "IB-C03H: HAL 826", score: "4" }
+];
+
+// 身體
+export const parts_body = [
+    { id: "01", name: "AC-J-120 BASHO", score: "4" },
+    { id: "02", name: "AC-J-120/RC JAILBREAK", score: "-1" },
+    { id: "03", name: "BD-011 MELANDER", score: "2" },
+    { id: "04", name: "BD-012 MELANDER C3", score: "4" },
+    { id: "05", name: "DF-BD-08 TIAN-QIANG", score: "4" },
+    { id: "06", name: "VP-40S", score: "3" },
+    { id: "07", name: "NACHTREIHER/40E", score: "3" },
+    { id: "08", name: "LAMMERGEIER/40F", score: "5" },
+    { id: "09", name: "VE-40A", score: "5" },
+    { id: "10", name: "CC-2000 ORBITER", score: "0" },
+    { id: "11", name: "CC-3000 WRECKER", score: "2" },
+    { id: "12", name: "CS-5000 MAIN DISH", score: "5" },
+    { id: "13", name: "EL-TC-10 FIRMEZA", score: "0" },
+    { id: "14", name: "EL-PC-00 ALBA", score: "2" },
+    { id: "15", name: "07-061 MIND ALPHA", score: "5" },
+    { id: "16", name: "IA-C01C: EPHEMERA", score: "1" },
+    { id: "17", name: "IB-C03C: HAL 826", score: "4" }
+];
+
+// 手部
+export const parts_hand = [
+    { id: "01", name: "AA-J-123 BASHO", score: "5" },
+    { id: "02", name: "AA-J-123/RC JAILBREAK", score: "-1" },
+    { id: "03", name: "AR-011 MELANDER", score: "3" },
+    { id: "04", name: "AR-012 MELANDER C3", score: "4" },
+    { id: "05", name: "DF-AR-08 TIAN-QIANG", score: "4" },
+    { id: "06", name: "DF-AR-09 TIAN-LAO", score: "5" },
+    { id: "07", name: "VP-46S", score: "2" },
+    { id: "08", name: "VP-46D", score: "3" },
+    { id: "09", name: "NACHTREIHER/46E", score: "4" },
+    { id: "10", name: "LAMMERGEIER/46F", score: "3" },
+    { id: "11", name: "VE-46A", score: "4" },
+    { id: "12", name: "AC-2000 TOOL ARM", score: "0" },
+    { id: "13", name: "AC-3000 WRECKER", score: "1" },
+    { id: "14", name: "AS-5000 SALAD", score: "2" },
+    { id: "15", name: "EL-TA-10 FIRMEZA", score: "2" },
+    { id: "16", name: "EL-PA-00 ALBA", score: "4" },
+    { id: "17", name: "04-101 MIND ALPHA", score: "3" },
+    { id: "18", name: "IA-C01A: EPHEMERA", score: "5" },
+    { id: "19", name: "IB-C03A: HAL 826", score: "2" }
+];
+
+// 腿部
+export const parts_leg = [
+    { id: "01", name: "AL-J-121 BASHO", score: "1" },
+    { id: "02", name: "AL-J-121/RC JAILBREAK", score: "-1" },
+    { id: "03", name: "LG-011 MELANDER", score: "3" },
+    { id: "04", name: "LG-012 MELANDER C3", score: "4" },
+    { id: "05", name: "DF-LG-08 TIAN-QIANG", score: "4" },
+    { id: "06", name: "VP-422", score: "3" },
+    { id: "07", name: "NACHTREIHER/42E", score: "5" },
+    { id: "08", name: "VE-42A", score: "5" },
+    { id: "09", name: "2C-2000 CRAWLER", score: "0" },
+    { id: "10", name: "2C-3000 WRECKER", score: "2" },
+    { id: "11", name: "2S-5000 DESSERT", score: "4" },
+    { id: "12", name: "EL-TL-10 FIRMEZA", score: "1" },
+    { id: "13", name: "EL-PL-00 ALBA", score: "3" },
+    { id: "14", name: "06-041 MIND ALPHA", score: "5" },
+    { id: "15", name: "IA-C01L: EPHEMERA", score: "0" },
+    { id: "16", name: "IB-C03L: HAL 826", score: "3" },
+    { id: "17", name: "KASUAR/42Z", score: "1" },
+    { id: "18", name: "RC-2000 SPRING CHICKEN", score: "0" },
+    { id: "19", name: "06-042 MIND BETA", score: "4" },
+    { id: "20", name: "LG-033M VERRILL", score: "4" },
+    { id: "21", name: "VP-424", score: "5" },
+    { id: "22", name: "LAMMERGEIER/42F", score: "5" },
+    { id: "23", name: "LG-022T BORNEMISSZA", score: "8" },
+    { id: "24", name: "VE-42B", score: "8" },
+    { id: "25", name: "EL-TL-11 FORTALEZA", score: "6" }
+];
+
+// 推進器
+export const booster = [
+    { id: "01", name: "AB-J-137 KIKAKU", score: "4" },
+    { id: "02", name: "BST-G1/P10", score: "5" },
+    { id: "03", name: "BST-G2/P04", score: "2" },
+    { id: "04", name: "BST-G2/P06SPD", score: "5" },
+    { id: "05", name: "ALULA/21E", score: "5" },
+    { id: "06", name: "FLUEGEL/21Z", score: "5" },
+    { id: "07", name: "BUERZEL/21D", score: "5" },
+    { id: "08", name: "BC-0600 12345", score: "5" },
+    { id: "09", name: "BC-0400 MULE", score: "-1" },
+    { id: "10", name: "BC-0200 GRIDWALKER", score: "4" },
+    { id: "11", name: "IA-C01B: GILLS", score: "1" },
+    { id: "12", name: "IB-C03B: NGI 001", score: "4" }
+];
+
+// 火控系統
+export const fire_control_system = [
+    { id: "01", name: "FCS-G1/P01", score: "-1" },
+    { id: "02", name: "FCS-G2/P05", score: "2" },
+    { id: "03", name: "FCS-G2/P10SLT", score: "5" },
+    { id: "04", name: "FCS-G2/P12SML", score: "0" },
+    { id: "05", name: "FC-006 ABBOT", score: "5" },
+    { id: "06", name: "FC-008 TALBOT", score: "5" },
+    { id: "07", name: "VE-21A", score: "4" },
+    { id: "08", name: "VE-21B", score: "1" },
+    { id: "09", name: "IA-C01F: OCELLUS", score: "5" },
+    { id: "10", name: "IB-C03F: WLT 001", score: "4" }
+];
+
+// 發電機
+export const generators = [
+    { id: "01", name: "AG-J-098 JOSO", score: "-1" },
+    { id: "02", name: "AG-E-013 YABA", score: "4" },
+    { id: "03", name: "AG-T-005 HOKUSHI", score: "5" },
+    { id: "04", name: "DF-GN-02 LING-TAI", score: "-1" },
+    { id: "05", name: "DF-GN-06 MING-TANG", score: "5" },
+    { id: "06", name: "DF-GN-08 SAN-TAI", score: "4" },
+    { id: "07", name: "VP-20S", score: "0" },
+    { id: "08", name: "VP-20C", score: "4" },
+    { id: "09", name: "VP-20D", score: "5" },
+    { id: "10", name: "VE-20A", score: "0" },
+    { id: "11", name: "VE-20B", score: "5" },
+    { id: "12", name: "VE-20C", score: "5" },
+    { id: "13", name: "IA-C01G AORTA", score: "3" },
+    { id: "14", name: "IB-C03G NGI 000", score: "5" }
+];
